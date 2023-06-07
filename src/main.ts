bc6d707b486b241344b10323a27215562e9fef5a
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-06-07 19:17:41 -0400

    Redoing html

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
 import './style.css'
 
+// // Buttons and result element
+// const rockBtns = document.querySelectorAll('.rock-btn')
+
+// const paperBtns = document.querySelectorAll('.paper-btn')
+
+// const scissorsBtns = document.querySelectorAll('.scissors-btn')
+
+// const resultElement = document.querySelectorAll('result')
+
+// const resetButton = document.getElementById('result')
+
+// const resetButton = document.getElementById('reset-btn')
+
+// // Add event listeners to all buttons
+// rockBtns.forEach(btn => btn.addEventListener('click', () => handleClick('rock')))
+
+
+
+
+
